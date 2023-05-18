fc64257ad75a4baca69aab1ab673c5a4dbfa47e7
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2023-05-18 11:55:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿if (args.Length == 0) { System.Console.WriteLine("There were no Arguments passed. Please try again"); }
+;
+void Main()
+{
+  
+
+      string SearchPath = args[0];
+      string SearchPattern = args[1];
+      string SearchWord = args[2];
+      string[] lines = File.ReadAllLines(SearchPath);
+      bool containsSearchString = File.ReadAllText(SearchPath).Contains(SearchWord);
+      if (containsSearchString)
+      {
+         for(int j = 0; j < lines.Length; j++)
+         {
+            if (lines[j].Contains(SearchWord))
+            {
+               System.Console.WriteLine(lines[j]);
+            }
+         }
+         System.Console.WriteLine(Directory.GetFiles(SearchPath, SearchPattern, SearchOption.AllDirectories).FirstOrDefault());
+         
+        
+
+      }
+      else 
+      {
+         System.Console.WriteLine("Your search word was not found in the files");
+      }
+   }
+
+Main();
